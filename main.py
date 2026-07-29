cf355b6bd53256cc1fdeb26932a7617ee265635b
deshmukhvrushali-oss <deshmukh.vrushali@universal.edu.in>
2026-07-29 21:32:05 +0530

    Create project structure

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Agentic RAG Short Video Generator")
+
+print("=" * 50)
+print("Agentic RAG Short Video Generator")
+print("=" * 50)
+
+topic = input("Enter Topic: ")
+
+print(f"\nResearch Started for: {topic}")
